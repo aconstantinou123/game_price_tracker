20652f658d0cb67517707adfdc8c1bdf91718635
Alex Constantinou <a_constantinou@hotmail.co.uk>
2021-12-27 18:13:46 +0000

    totals sheet added, cmd args used and condition column updated

--- Makefile
+++
@@ -1,8 +0,0 @@
-include .env
-export
-
-clean:
-	rm spreadsheets/$(OUTPUT_FILE_NAME)
-
-run: clean
-	python app.py
--- app.py
+++ app.py
@@ -2,48 +2,97 @@ import os
 import pandas as pd
 import requests
 from urllib import parse
+from sys import argv
 from bs4 import BeautifulSoup
 from currency_converter import CurrencyConverter
-from dotenv import load_dotenv
 
-load_dotenv()
 
-INPUT_FILE_NAME = os.getenv('INPUT_FILE_NAME')
-OUTPUT_FILE_NAME = os.getenv('OUTPUT_FILE_NAME')
+if len(argv) < 3:
+    raise AttributeError('Missing args')
+
+INPUT_FILE_PATH = argv[1]
+OUTPUT_FILE_PATH = argv[2]
+try:
+    CURRENCY = argv[3]
+except IndexError:
+    CURRENCY = 'USD'
 
 c = CurrencyConverter()
 
-workbook = pd.ExcelFile(f'spreadsheets/{INPUT_FILE_NAME}')
+workbook = pd.ExcelFile(f'{INPUT_FILE_PATH}')
 sheets = workbook.sheet_names
 df = pd.concat([pd.read_excel(workbook, sheet_name=s)
-                .assign(sheet_name=s) for s in sheets])
+                .assign(Platform=s) for s in sheets])
 
 prices = []
+print('Collecting prices from www.pricecharting.com')
 for index, row in df.iterrows():
     title = parse.quote(row['Title'].lower().replace(' ', '-'))
-    console = row['sheet_name'].lower().replace(' ', '-')
+    console = row['Platform'].lower().replace(' ', '-')
     region = row['Region'].lower()
-    platform = f'{region}-{console}' if region == 'pal' else console
+
+    platform = console
+    if region == 'pal':
+        platform = f'{region}-{console}'
+    elif region == 'ntsc-j':
+        platform = f'jp-{console}'
+
     url = f'https://www.pricecharting.com/game/{platform}/{title}'
     r = requests.get(url)
 
     data = r.text
     soup = BeautifulSoup(data, features='html.parser')
-    price_html_id = 'complete_price' if row['CIB'] == 'X' else 'used_price'
+
+    price_html_id = '' 
+    if row['Condition'] == 'C':
+        price_html_id = 'complete_price'
+    elif row['Condition'] == 'L':
+        price_html_id == 'used_price'
+    elif row['Condition'] == 'N':
+         price_html_id == 'new_price'
+    elif row['Condition'] == 'G':
+         price_html_id == 'graded_price'
+    elif row['Condition'] == 'B':
+         price_html_id == 'box_only_price'
+    elif row['Condition'] == 'M':
+         price_html_id == 'manual_only_price'
+
     try:
         price_html = soup.find(id=price_html_id)
         price_text = price_html.find(class_='price js-price').text
-        us_price = float(("".join(line.strip() 
+        price = float(("".join(line.strip() 
                     for line in price_text.split("\n")).replace('$', '')))
-        gbp_price = round(c.convert(us_price, 'USD', 'GBP'), 2)
-        prices.append(float(gbp_price))
+
+        if CURRENCY != 'USD':
+            price = round(c.convert(price, 'USD', CURRENCY), 2)
+
+        prices.append(price)
     except AttributeError:
         price = 0.00
         print('Missing price', row['Title'])
         prices.append(price)
-df['Price (GBP)'] = prices
 
-writer = pd.ExcelWriter(f'spreadsheets/{OUTPUT_FILE_NAME}', engine='xlsxwriter')
-for group, data in df.groupby('sheet_name'):
-    data.to_excel(writer, group, float_format="%.2f", index=False)
+print('Prices collected')
+df[f'Price ({CURRENCY})'] = prices
+
+print(f'Writing results to {OUTPUT_FILE_PATH}')
+writer = pd.ExcelWriter(f'{OUTPUT_FILE_PATH}', engine='xlsxwriter')
+for group, data in df.groupby('Platform'):
+    data.drop(columns=['Platform'], inplace=True)
+    data.to_excel(writer, group, index=False)
+
+workbook  = writer.book
+price_format = workbook.add_format({'num_format': '0.00'})
+sheets = writer.sheets
+for sheet in sheets:
+    worksheet = writer.sheets[sheet]
+    worksheet.set_column('D:D', None, price_format)
+
+totals_df = pd.DataFrame(df.groupby('Platform').sum())
+totals_df.loc['Total']= df[f'Price ({CURRENCY})'].sum()
+totals_df.to_excel(writer, sheet_name='Totals')
+totals_worksheet = writer.sheets['Totals']
+totals_worksheet.set_column('B:B', None, price_format)
+
 writer.save()
+print('Done!')
