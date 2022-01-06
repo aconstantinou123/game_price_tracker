25ef0c89757919adb95f8576a8fa13212716637c
Alex Constantinou <a_constantinou@hotmail.co.uk>
2022-01-06 15:43:41 +0000

    switch to aiohttp lib to improve performance

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
-import requests
+import aiohttp
+import asyncio
 from urllib import parse
 from sys import argv
 from bs4 import BeautifulSoup
@@ -25,78 +26,89 @@ except IndexError:
 
 c = CurrencyConverter()
 
-workbook = pd.ExcelFile(f'{INPUT_FILE_PATH}')
-sheets = workbook.sheet_names
 
-df = pd.concat([pd.read_excel(workbook, sheet_name=s)
-                .assign(Platform=s) for s in sheets
-                if not PLATFORMS or s in PLATFORMS])
+async def get_price(session, url, row):
+    async with session.get(url) as r:
+        data = await r.text()
+        soup = BeautifulSoup(data, features='html.parser')
 
-prices = []
-print('Collecting prices from www.pricecharting.com')
-for index, row in df.iterrows():
-    title = parse.quote(row['Title'].lower().replace(' ', '-'))
-    console = row['Platform'].lower().replace(' ', '-')
-    region = row['Region'].lower()
+        condition_lookup = {
+            'C': 'complete_price',
+            'L': 'used_price',
+            'N': 'new_price',
+            'G': 'graded_price',
+            'B': 'box_only_price',
+            'M': 'manual_only_price',
+        }
+        price_html_id = condition_lookup[row['Condition']] 
+    
+        try:
+            price_html = soup.find(id=price_html_id)
+            price_text = price_html.find(class_='price js-price').text
+            price = float(("".join(line.strip() 
+                        for line in price_text.split("\n")).replace('$', '')))
 
-    platform = console
-    if region == 'pal':
-        platform = f'{region}-{console}'
-    elif region == 'ntsc-j':
-        platform = f'jp-{console}'
+            if CURRENCY != 'USD':
+                price = round(c.convert(price, 'USD', CURRENCY), 2)
+            # print(f'{row["Platform"]}, {row["Title"]}: {price} {CURRENCY}')
+            return price
+        except AttributeError:
+            price = 0.00
+            print('Missing price', row['Title'])
+            return price
 
-    url = f'https://www.pricecharting.com/game/{platform}/{title}'
-    r = requests.get(url)
 
-    data = r.text
-    soup = BeautifulSoup(data, features='html.parser')
+async def main():
+    async with aiohttp.ClientSession() as session:
+        workbook = pd.ExcelFile(f'{INPUT_FILE_PATH}')
+        sheets = workbook.sheet_names
 
-    condition_lookup = {
-        'C': 'complete_price',
-        'L': 'used_price',
-        'N': 'new_price',
-        'G': 'graded_price',
-        'B': 'box_only_price',
-        'M': 'manual_only_price',
-    }
-    price_html_id = condition_lookup[row['Condition']] 
-   
-    try:
-        price_html = soup.find(id=price_html_id)
-        price_text = price_html.find(class_='price js-price').text
-        price = float(("".join(line.strip() 
-                    for line in price_text.split("\n")).replace('$', '')))
+        df = pd.concat([pd.read_excel(workbook, sheet_name=s)
+                        .assign(Platform=s) for s in sheets
+                        if not PLATFORMS or s in PLATFORMS])
 
-        if CURRENCY != 'USD':
-            price = round(c.convert(price, 'USD', CURRENCY), 2)
-        # print(f'{row["Platform"]}, {row["Title"]}: {price} {CURRENCY}')
-        prices.append(price)
-    except AttributeError:
-        price = 0.00
-        print('Missing price', row['Title'])
-        prices.append(price)
+        tasks = []
+        print('Collecting prices from www.pricecharting.com')
+        for index, row in df.iterrows():
+            title = parse.quote(row['Title'].lower().replace(' ', '-'))
+            console = row['Platform'].lower().replace(' ', '-')
+            region = row['Region'].lower()
 
-print('Prices collected')
-df[f'Price ({CURRENCY})'] = prices
+            platform = console
+            if region == 'pal':
+                platform = f'{region}-{console}'
+            elif region == 'ntsc-j':
+                platform = f'jp-{console}'
 
-print(f'Writing results to {OUTPUT_FILE_PATH}')
-writer = pd.ExcelWriter(f'{OUTPUT_FILE_PATH}', engine='xlsxwriter')
-for group, data in df.groupby('Platform'):
-    data.drop(columns=['Platform'], inplace=True)
-    data.to_excel(writer, group, index=False)
+            url = f'https://www.pricecharting.com/game/{platform}/{title}'
 
-workbook  = writer.book
-price_format = workbook.add_format({'num_format': '0.00'})
-sheets = writer.sheets
-for sheet in sheets:
-    worksheet = writer.sheets[sheet]
-    worksheet.set_column('D:D', None, price_format)
+            tasks.append(asyncio.ensure_future(get_price(session, url, row)))
 
-totals_df = pd.DataFrame(df.groupby('Platform').sum())
-totals_df.loc['Total']= df[f'Price ({CURRENCY})'].sum()
-totals_df.to_excel(writer, sheet_name='Totals')
-totals_worksheet = writer.sheets['Totals']
-totals_worksheet.set_column('B:B', None, price_format)
+        prices = await asyncio.gather(*tasks)
 
-writer.save()
-print('Done!')
+        print('Prices collected')
+        df[f'Price ({CURRENCY})'] = prices
+
+        print(f'Writing results to {OUTPUT_FILE_PATH}')
+        writer = pd.ExcelWriter(f'{OUTPUT_FILE_PATH}', engine='xlsxwriter')
+        for group, data in df.groupby('Platform'):
+            data.drop(columns=['Platform'], inplace=True)
+            data.to_excel(writer, group, index=False)
+
+        workbook  = writer.book
+        price_format = workbook.add_format({'num_format': '0.00'})
+        sheets = writer.sheets
+        for sheet in sheets:
+            worksheet = writer.sheets[sheet]
+            worksheet.set_column('D:D', None, price_format)
+
+        totals_df = pd.DataFrame(df.groupby('Platform').sum())
+        totals_df.loc['Total']= df[f'Price ({CURRENCY})'].sum()
+        totals_df.to_excel(writer, sheet_name='Totals')
+        totals_worksheet = writer.sheets['Totals']
+        totals_worksheet.set_column('B:B', None, price_format)
+
+        writer.save()
+        print('Done!')
+
+asyncio.run(main())
